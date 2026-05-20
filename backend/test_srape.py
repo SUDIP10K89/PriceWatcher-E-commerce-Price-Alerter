0ba065ccd5e8ba8ac596205d810ce7df75b14a0a
SUDIP10K89 <sudip10k89@gmail.com>
2026-05-20 11:23:37 +0545

    feat: add initial scraping functionality for Daraz product

---
+++ backend/test_srape.py
@@ -0,0 +1,3 @@
+from scraper_manager import scrape_product
+
+scrape_product(4, "https://www.daraz.com.np/products/professional-noise-cancellation-clip-collar-mic-condenser-for-youtube-video-interviews-lectures-news-travel-videos-ideal-for-mobile-use-i111626830-s1030227632.html?scm=1007.51610.379274.0&pvid=0ce62b21-abba-4596-ae73-20262d11890d&search=flashsale&spm=a2a0e.tm80335409.FlashSale.d_111626830", "daraz")
